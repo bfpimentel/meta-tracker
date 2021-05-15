b5374f861b29e61c719b5d5c4cc37843e3c1250d
Bruno Pimentel <bruno@pimentel.dev>
2021-05-15 20:58:20 -0300

    feat: add dark mode

--- web/pages/[codes].tsx
+++ web/pages/[codes].tsx
@@ -1,42 +1,66 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
+import { useTheme } from "next-themes";
 import { track } from "@/data/repository";
 import { InferGetStaticPropsType } from "next";
 import moment from "moment";
 
 const Trackings = ({
   trackings,
-}: InferGetStaticPropsType<typeof getServerSideProps>) => (
-  <div className="flex flex-col items-center justify-center">
-    <Head>
-      <title>Meta Tracker</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+}: InferGetStaticPropsType<typeof getServerSideProps>) => {
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
 
-    <main className="flex flex-col max-w-3xl py-4 px-2">
-      <p className="text-4xl text-center font-semibold">Meta Tracker</p>
-      {trackings.map((tracking) => (
-        <div key={tracking.code}>
-          <div className="border-t mt-4"></div>
-          <p className="text-xl mt-4 font-bold">{tracking.code}</p>
-          {tracking.events.map((event, index) => (
-            <div key={index} className="mt-2">
-              <p className="text-gray-500">
-                {moment(event.trackedAt).format("DD/MM/yyyy [às] HH:mm")}
-              </p>
-              <p>{event.description}</p>
-            </div>
-          ))}
+  useEffect(() => {
+    setIsMounted(true);
+  });
+
+  const switchTheme = () => {
+    if (isMounted) {
+      setTheme(theme === "light" ? "dark" : "light");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Head>
+        <title>Meta Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col max-w-3xl py-4 px-2">
+        <div className="flex flex-row justify-between">
+          <p className="text-4xl text-center font-semibold">Meta Tracker</p>
+          <button
+            onClick={switchTheme}
+            className="p-2 rounded-md font-bold bg-black text-white dark:bg-white dark:text-black"
+          >
+            Give me {theme === "light" ? "Darkness" : "Light"}
+          </button>
         </div>
-      ))}
-    </main>
-  </div>
-);
+        {trackings.map((tracking) => (
+          <div key={tracking.code}>
+            <div className="mt-4 border-t border-black border-opacity-10 dark:border-white dark:border-opacity-10"></div>
+            <p className="text-xl mt-4 font-bold">{tracking.code}</p>
+            {tracking.events.map((event, index) => (
+              <div key={index} className="mt-2">
+                <p className="text-gray-500 dark:text-gray-400">
+                  {moment(event.trackedAt).format("DD/MM/yyyy [às] HH:mm")}
+                </p>
+                <p>{event.description}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+};
 
 export const getServerSideProps = async (context: {
   params: { codes: string };
 }) => {
   const splitCodes = context.params.codes.split(",");
-
   const trackings = await track(splitCodes);
 
   return {
