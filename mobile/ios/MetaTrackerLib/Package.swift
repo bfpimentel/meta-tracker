41f0582c6e4394945dc4b232ea743b496dab1d91
Guilherme Souza <grsouza@pm.me>
2021-05-19 06:30:43 -0300

    feat(ios): add DatabaseClient target

--- mobile/ios/MetaTrackerLib/Package.swift
+++ mobile/ios/MetaTrackerLib/Package.swift
@@ -22,6 +22,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "DatabaseClient"),
         .target(
             name: "MetaTrackerLib",
             dependencies: []),
